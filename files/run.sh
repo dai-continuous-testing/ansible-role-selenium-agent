da7fb22928ce81d212634f88d080e0bb33a0e4ba
shai atias <shai@experitest.com>
2019-04-02 21:21:45 +0300

    REMOVE old file

--- files/run.sh
+++
@@ -1,3 +0,0 @@
-
-# will not create duplicated rows by name
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "selenium_agent_server", path:"/Users/jenkins/SeleniumAgent/selenium_agent_server", hidden:false}'
